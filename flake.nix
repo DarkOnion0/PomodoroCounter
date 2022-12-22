7799a8a7f3a08dc545046fafe9b543ebfd3b9949
DarkOnion0 <darkonion0@clapot.org>
2022-12-22 09:34:55 +0100

    feat: init project
    feat: init project

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
     devShell = pkgs.mkShell {
       CARGO_INSTALL_ROOT = "${toString ./.}/.cargo";
 
-      buildInputs = with pkgs; [ cargo rustc git ];
+      buildInputs = with pkgs; [ git rustup ];
     };
   });
 }

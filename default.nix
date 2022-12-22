0e1fc348fc2b34906175fb7251e0a36c4649436c
DarkOnion0 <darkonion0@clapot.org>
2022-12-22 13:37:28 +0100

    build(nix): update flake

--- default.nix
+++ default.nix
@@ -1,37 +1,38 @@
-{ pkgs ? import <nixpkgs> { }
-, stdenv ? pkgs.stdenv
-, lib ? pkgs.lib
-# A set providing `buildRustPackage :: attrsets -> derivation`
-, rustPlatform ? pkgs.rustPlatform
-, fetchFromGitHub ? pkgs.fetchFromGitHub
-, gitignoreSrc ? null
-}:
-
-let
+{
+  pkgs ? import <nixpkgs> {},
+  stdenv ? pkgs.stdenv,
+  lib ? pkgs.lib,
+  # A set providing `buildRustPackage :: attrsets -> derivation`
+  rustPlatform ? pkgs.rustPlatform,
+  fetchFromGitHub ? pkgs.fetchFromGitHub,
+  gitignoreSrc ? null,
+}: let
   gitignoreSource =
     if gitignoreSrc != null
     then gitignoreSrc.gitignoreSource
-    else (import (fetchFromGitHub {
-      owner = "hercules-ci";
-      repo = "gitignore";
-      rev = "c4662e662462e7bf3c2a968483478a665d00e717";
-      sha256 = "0jx2x49p438ap6psy8513mc1nnpinmhm8ps0a4ngfms9jmvwrlbi";
-    }) { inherit lib; }).gitignoreSource;
+    else
+      (import (fetchFromGitHub {
+        owner = "hercules-ci";
+        repo = "gitignore";
+        rev = "c4662e662462e7bf3c2a968483478a665d00e717";
+        sha256 = "0jx2x49p438ap6psy8513mc1nnpinmhm8ps0a4ngfms9jmvwrlbi";
+      }) {inherit lib;})
+      .gitignoreSource;
 in
-rustPlatform.buildRustPackage rec {
-  pname = "pomodoro_counter";
-  version = "0.1.0";
+  rustPlatform.buildRustPackage rec {
+    pname = "pomodoro_counter";
+    version = "0.1.0";
 
-  src = gitignoreSource ./.;
+    src = gitignoreSource ./.;
 
-  cargoSha256 = "sha256-Eha/5TRcmHuxmxtcTHu/xKAZuWERRmzj3U1dnfTlbZU=";
+    cargoSha256 = "sha256-Eha/5TRcmHuxmxtcTHu/xKAZuWERRmzj3U1dnfTlbZU=";
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-  };
+    cargoLock = {
+      lockFile = ./Cargo.lock;
+    };
 
-  meta = with lib; {
-    description = "An app to convert pomodoros to real hours";
-    license = licenses.agpl3;
-  };
-}
+    meta = with lib; {
+      description = "An app to convert pomodoros to real hours";
+      license = licenses.agpl3;
+    };
+  }
--- flake.nix
+++ flake.nix
@@ -4,40 +4,47 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # We want to use packages from the binary cache
     flake-utils.url = "github:numtide/flake-utils";
-    gitignore = { url = "github:hercules-ci/gitignore.nix"; flake = false; };
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      flake = false;
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
-  flake-utils.lib.eachSystem [ flake-utils.lib.system.x86_64-linux flake-utils.lib.system.aarch64-linux flake-utils.lib.system.i686-linux ] (system: let
-    inherit (builtins) substring;
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachSystem [flake-utils.lib.system.x86_64-linux flake-utils.lib.system.aarch64-linux flake-utils.lib.system.i686-linux] (system: let
+      inherit (builtins) substring;
 
-    # to work with older version of flakes
-    lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+      # to work with older version of flakes
+      pkgs = nixpkgs.legacyPackages.${system};
+      gitignoreSrc = pkgs.callPackage inputs.gitignore {};
+    in rec {
+      packages = rec {
+        default = pomodoro_counter;
 
-    # Generate a user-friendly version number.
-    version = builtins.substring 0 8 lastModifiedDate;
-
-    pkgs = nixpkgs.legacyPackages.${system};
-    gitignoreSrc = pkgs.callPackage inputs.gitignore { };
-  in rec {
-    packages.pomodoro_counter = pkgs.callPackage ./default.nix { inherit gitignoreSrc; };
-
-    legacyPackages = packages;
-
-    defaultPackage = packages.pomodoro_counter;
-
-    devShell = pkgs.mkShell {
-      CARGO_INSTALL_ROOT = "${toString ./.}/.cargo";
-
-      buildInputs = with pkgs; [ git rustup ];
-    };
-
-    apps = rec {
-      default = pomodoro_counter;
-      pomodoro_counter = {
-        type = "app";
-        program = "${self.packages.${system}.default}/bin/pomodoro_counter";
+        pomodoro_counter = pkgs.callPackage ./default.nix {inherit gitignoreSrc;};
       };
-    };
-  });
+
+      legacyPackages = packages;
+
+      defaultPackage = packages.pomodoro_counter;
+
+      devShell = pkgs.mkShell {
+        CARGO_INSTALL_ROOT = "${toString ./.}/.cargo";
+
+        buildInputs = with pkgs; [git rustup alejandra];
+      };
+
+      apps = rec {
+        default = pomodoro_counter;
+        pomodoro_counter = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/pomodoro_counter";
+        };
+      };
+    });
 }

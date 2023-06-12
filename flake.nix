38925252655bdf5862bf90edaed3254c397b6059
DarkOnion0 <darkonion0@clapot.org>
2023-06-12 15:02:54 +0200

    build(nix): update flake

--- flake.nix
+++ flake.nix
@@ -4,19 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # We want to use packages from the binary cache
 
-    flake-utils.url = "github:numtide/flake-utils";
-
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs = {
-        flake-utils.follows = "flake-utils";
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
 
     devenv.url = "github:cachix/devenv";
   };
@@ -24,74 +12,63 @@
   outputs = inputs @ {
     self,
     nixpkgs,
-    flake-utils,
+    flake-parts,
     fenix,
-    crane,
     devenv,
     ...
   }:
-    flake-utils.lib.eachSystem [flake-utils.lib.system.x86_64-linux flake-utils.lib.system.aarch64-linux flake-utils.lib.system.i686-linux] (system: let
-      inherit (builtins) substring;
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      debug = true;
+      systems = ["x86_64-linux" "aarch64-linux"];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        devShells.default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              env.CARGO_INSTALL_ROOT = "${toString ./.}/.cargo";
 
-      # to work with older version of flakes
-      pkgs = nixpkgs.legacyPackages.${system};
+              languages.rust.enable = true;
 
-      rust-toolchain = fenix.packages.${system}.stable;
+              packages = with pkgs; [
+                git
+                alejandra
+              ];
 
-      craneLib = crane.lib.${system}.overrideToolchain rust-toolchain.completeToolchain;
-    in rec {
-      packages = rec {
-        default = pomodoro_counter;
+              scripts = {
+                run-dev.exec = "cargo run -- ";
+                run-prod.exec = "nix run .# -- ";
+              };
 
-        pomodoro_counter = craneLib.buildPackage {
-          src = ./.;
+              pre-commit.hooks = {
+                # Rust
+                clippy.enable = true;
+                rustfmt.enable = true;
+                cargo-check.enable = true;
+
+                # Nix
+                alejandra.enable = true;
+
+                # Markdown...
+                prettier.enable = true;
+              };
+            }
+          ];
+        };
+
+        apps = rec {
+          default = pomodoro_counter;
+          pomodoro_counter = {
+            type = "app";
+            program = "${self.packages.${system}.default}/bin/pomodoro_counter";
+          };
         };
       };
-
-      legacyPackages = packages;
-
-      defaultPackage = packages.pomodoro_counter;
-
-      devShell = devenv.lib.mkShell {
-        inherit inputs pkgs;
-        modules = [
-          {
-            env.CARGO_INSTALL_ROOT = "${toString ./.}/.cargo";
-
-            packages = with pkgs; [
-              git
-              alejandra
-              rust-toolchain.completeToolchain
-              rust-analyzer
-            ];
-
-            scripts = {
-              run-dev.exec = "cargo run -- ";
-              run-prod.exec = "nix run .# -- ";
-            };
-
-            pre-commit.hooks = {
-              # Rust
-              clippy.enable = true;
-              rustfmt.enable = true;
-              cargo-check.enable = true;
-
-              # Nix
-              alejandra.enable = true;
-
-              # Markdown...
-              prettier.enable = true;
-            };
-          }
-        ];
-      };
-
-      apps = rec {
-        default = pomodoro_counter;
-        pomodoro_counter = {
-          type = "app";
-          program = "${self.packages.${system}.default}/bin/pomodoro_counter";
-        };
-      };
-    });
+    };
 }
